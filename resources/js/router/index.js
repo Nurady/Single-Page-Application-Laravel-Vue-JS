681de320f1d336789d6fea57b780b72d76bfe91e
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-10-30 11:33:11 +0800

    first commit

---
+++ resources/js/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import beforeEach from "./beforeEach";
+
+import auth from "../app/auth/router";
+import home from "../app/home/router";
+import dashboard from "../app/dashboard/router";
+import errors from "../app/errors/router";
+
+const routes = [...auth, ...home, ...dashboard, ...errors];
+
+Vue.use(VueRouter);
+console.log(routes);
+
+const router = new VueRouter({
+    mode: "history",
+    routes
+});
+
+router.beforeEach(beforeEach);
+export default router;
